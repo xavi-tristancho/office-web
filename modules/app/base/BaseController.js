bb24660d0fc3739fa3cba5897d29d6677fa9d073
Xavier Tristancho <xavi@letitguide.com>
2015-12-06 20:43:23 +0100

    General Refactoring, translations & base controller creation

--- modules/app/base/BaseController.js
+++ modules/app/base/BaseController.js
@@ -1,6 +1,6 @@
 module.exports = function(ngModule) {
 
-    function BaseController(DTOptionsBuilder, datatablesLanguages, $translate)
+    function BaseController(DTOptionsBuilder, datatablesLanguages, $translate, $filter)
     {
         var vm = this;
         vm.order = [[0, "asc"]];
@@ -12,6 +12,18 @@ module.exports = function(ngModule) {
                 .fromSource(getAll()).withBootstrap()
                 .withOption('language', eval('datatablesLanguages.' + language))
                 .withOption('order', vm.order);
+        }
+
+        this.getCustomerFullName = function(customer)
+        {
+            if(typeof customer != 'undefined' && customer != null)
+                return customer.name + " " + customer.first_name + " " + customer.second_name;
+        }
+
+        this.getLocalizedDate = function(timestamp)
+        {
+            var at = $filter('translate')('fields.at');
+            return $filter('date')(timestamp, "MM/dd/yyyy '" + at  + "' H:mm");
         }
     }
 
